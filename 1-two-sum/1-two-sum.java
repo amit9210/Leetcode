cedc13756f7c4352b4dca61dcc88c77b57107959
amit9210 <102462435+amit9210@users.noreply.github.com>
2022-09-04 10:50:28 +0530

    Time: 10 ms (49.85%), Space: 46.6 MB (11.12%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,8 +1,5 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int start =0;
-        int end=1;
-        int sum=0;
         int a[]= new int[2];
         HashMap<Integer,Integer> map= new HashMap<>();
         for(int i=0;i<nums.length;i++){
